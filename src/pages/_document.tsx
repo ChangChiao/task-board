2ed9dea8f09f0af02c5e3c53b410cfb61baea9ef
joe <joe.chang1014@gmail.com>
2022-06-09 10:42:38 +0800

    feat: add font

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -7,7 +7,12 @@ class MyDocument extends Document {
   render() {
     return (
       <Html lang={AppConfig.locale}>
-        <Head />
+        <Head>
+          <link
+            href="https://fonts.googleapis.com/css2?family=Outfit:wght@100&display=swap"
+            rel="stylesheet"
+          />
+        </Head>
         <body>
           <Main />
           <NextScript />
