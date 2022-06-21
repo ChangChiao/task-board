8f5e7fa516e84921fe4def41210dd6ae86754715
joe <joe.chang1014@gmail.com>
2022-06-22 07:31:01 +0800

    feat: add fb buttom

--- src/components/SignUp.tsx
+++ src/components/SignUp.tsx
@@ -1,16 +1,21 @@
 import { FcGoogle } from 'react-icons/fc';
+import { GrFacebook } from 'react-icons/gr';
 
 import { BASE_URL } from '../config';
 
-const oauthPath = `${BASE_URL}/auth/google`;
-
+const oauthGoogle = `${BASE_URL}/auth/google`;
+const oauthFB = `${BASE_URL}/auth/facebook`;
 const SignUp = () => {
   return (
     <div>
       <button>
-        <a className="flex items-center" href={oauthPath}>
+        <a className="flex items-center" href={oauthGoogle}>
           <FcGoogle className="mr-2" />
           Sign up with Google
+        </a>
+        <a className="flex items-center" href={oauthFB}>
+          <GrFacebook className="mr-2" />
+          Sign up with FaceBook
         </a>
       </button>
     </div>
