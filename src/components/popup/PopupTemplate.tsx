ead7332ea757c8dbe3296cf2d5ddc820bddf73a6
joe <joe.chang1014@gmail.com>
2022-07-12 17:07:48 +0800

    feat popup

--- src/components/popup/PopupTemplate.tsx
+++ src/components/popup/PopupTemplate.tsx
@@ -7,11 +7,7 @@ type PopupType = {
   children?: ReactNode | null;
 };
 
-const PopupTemplate: FC<PopupType> = ({
-  children,
-}: // setPopup,
-// isPopupShow,
-PopupType) => {
+const PopupTemplate: FC<PopupType> = ({ children }: PopupType) => {
   const { isPopupShow, setPopup } = usePopupContext();
   const handlePop = () => {
     setPopup(!isPopupShow);
@@ -20,31 +16,31 @@ PopupType) => {
     <div
       id="authentication-modal"
       aria-hidden="true"
-      className="fixed top-0 left-0 right-0 z-50 hidden w-full overflow-x-hidden overflow-y-auto md:inset-0 h-modal md:h-full"
+      className="fixed top-0 left-0 right-0 z-50 w-full h-full"
     >
-      <div className="relative w-full h-full max-w-md p-4 md:h-auto">
-        <div className="relative bg-white rounded-lg shadow dark:bg-gray-700">
-          <button
-            type="button"
-            className="absolute top-3 right-2.5 text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm p-1.5 ml-auto inline-flex items-center dark:hover:bg-gray-800 dark:hover:text-white"
-            data-modal-toggle="authentication-modal"
-            onClick={handlePop}
+      <div className="w-full h-full bg-black/50"></div>
+      <div className=" absolute h-[400px] p-2 top-0 right-0 left-0 bottom-0 m-auto bg-white rounded-lg w-1/3 max-w-[300px] shadow">
+        <h3 className="pt-2 text-xl text-center">test</h3>
+        <button
+          type="button"
+          className="absolute top-3 right-2.5 text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm p-1.5 ml-auto inline-flex items-center"
+          data-modal-toggle="authentication-modal"
+          onClick={handlePop}
+        >
+          <svg
+            className="w-5 h-5"
+            fill="currentColor"
+            viewBox="0 0 20 20"
+            xmlns="http://www.w3.org/2000/svg"
           >
-            <svg
-              className="w-5 h-5"
-              fill="currentColor"
-              viewBox="0 0 20 20"
-              xmlns="http://www.w3.org/2000/svg"
-            >
-              <path
-                fillRule="evenodd"
-                d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z"
-                clipRule="evenodd"
-              ></path>
-            </svg>
-          </button>
-          {children}
-        </div>
+            <path
+              fillRule="evenodd"
+              d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z"
+              clipRule="evenodd"
+            ></path>
+          </svg>
+        </button>
+        {children}
       </div>
     </div>
   );
--- src/components/popup/TaskAddPop.tsx
+++ src/components/popup/TaskAddPop.tsx
@@ -1,23 +1,73 @@
 import { FC, useState } from 'react';
 
+import { yupResolver } from '@hookform/resolvers/yup';
 import DatePicker from 'react-datepicker';
+import { SubmitHandler, useForm } from 'react-hook-form';
+import * as Yup from 'yup';
 
 import 'react-datepicker/dist/react-datepicker.css';
 import { usePopupContext } from '../../hooks/usePopupContext';
 // eslint-disable-next-line import/no-cycle
 import PopupTemplate from './PopupTemplate';
 
+type FormValues = {
+  title: string;
+  reward: number;
+  content: string;
+};
+
+const validationSchema = Yup.object().shape({
+  title: Yup.string().required('標題為必填'),
+  reward: Yup.string().required('酬勞為必填'),
+  content: Yup.string().required('內容為必填'),
+});
+
 const TaskAddPop: FC = () => {
   const { isPopupShow } = usePopupContext();
+  const {
+    register,
+    handleSubmit,
+    // reset,
+    formState: { errors },
+  } = useForm<FormValues>({
+    resolver: yupResolver(validationSchema),
+  });
   const [startDate, setStartDate] = useState(new Date());
+  const onSubmit: SubmitHandler<FormValues> = (data) => {
+    console.log(data);
+    // if (data.remember) {
+    //   localStorage.setItem('email', data.email);
+    // }
+  };
   return (
     <>
       {isPopupShow && (
         <PopupTemplate>
-          <DatePicker
-            selected={startDate}
-            onChange={(date: Date) => setStartDate(date)}
-          />
+          <form action="" onSubmit={handleSubmit(onSubmit)}>
+            <input className="field" {...register('title')} />
+            {errors.title?.message && (
+              <span className="text-sm text-red-500">
+                {errors.title?.message}
+              </span>
+            )}
+            <input className="field" {...register('reward')} />
+            {errors.reward?.message && (
+              <span className="text-sm text-red-500">
+                {errors.reward?.message}
+              </span>
+            )}
+            <textarea {...register('content')} />
+            {errors.content?.message && (
+              <span className="text-sm text-red-500">
+                {errors.content?.message}
+              </span>
+            )}
+            <DatePicker
+              selected={startDate}
+              onChange={(date: Date) => setStartDate(date)}
+            />
+            <input className="btn" type="submit" />
+          </form>
         </PopupTemplate>
       )}
     </>
