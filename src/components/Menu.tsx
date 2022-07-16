494086494d5ddb6426f0f41ed9cd870ec850e30b
joe <joe.chang1014@gmail.com>
2022-07-16 16:43:10 +0800

    feat menu list

--- src/components/Menu.tsx
+++ src/components/Menu.tsx
@@ -1,19 +1,13 @@
 import Link from 'next/link';
 
+import { MENU } from '../config';
 import Avatar from './atoms/Avatar';
 
 const Menu = () => {
-  const menuList = [
-    { text: '我要發任務', link: '' },
-    { text: '我接的任務', link: '' },
-    { text: '收藏清單', link: '' },
-    { text: '個人資訊', link: '' },
-    { text: '登出', link: '' },
-  ];
-  const userList = [
-    { text: '個人資訊', link: '' },
-    { text: '登出', link: '' },
-  ];
+  // const userList = [
+  //   { text: '個人資訊', link: '' },
+  //   { text: '登出', link: '' },
+  // ];
   return (
     <div className="w-[300px] p-4 text-white h-screen bg-cyan-900">
       <img
@@ -21,18 +15,18 @@ const Menu = () => {
         src="/assets/logo/logo_size_invert.jpg"
         alt=""
       />
+      <div className="flex items-center py-8">
+        <Avatar image={'/assets/avatar/1.png'} />
+        <span className="pl-3">userName</span>
+      </div>
       <ul>
-        {menuList.map((item) => (
+        {MENU.map((item) => (
           <li className="py-2" key={item.text}>
             <Link href={item.link}>{item.text}</Link>
           </li>
         ))}
       </ul>
-      <div className="pt-10">
-        <div className="flex items-center">
-          <Avatar image={'/assets/avatar/1.png'} />
-          <span>userName</span>
-        </div>
+      {/* <div className="pt-10">
         <ul>
           {userList.map((item) => (
             <li key={item.text}>
@@ -40,7 +34,7 @@ const Menu = () => {
             </li>
           ))}
         </ul>
-      </div>
+      </div> */}
     </div>
   );
 };
