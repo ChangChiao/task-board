1ebbdab67dd0f74e42e1d50a5964594eda3b3829
joe <joe.chang1014@gmail.com>
2022-09-05 14:27:21 +0800

    feat line pay btn

--- src/components/LinePay.tsx
+++ src/components/LinePay.tsx
@@ -1,5 +1,15 @@
+import { createOrder } from '../utils/http/linePay';
+
 const LinePay = () => {
-  return <div>123</div>;
+  const genOrder = async () => {
+    await createOrder();
+  };
+  return (
+    <div>
+      123
+      <button onClick={genOrder}>確認訂購</button>
+    </div>
+  );
 };
 
 export default LinePay;
---
+++ src/utils/http/linePay.ts
@@ -0,0 +1,14 @@
+import { BASE_URL } from '../../config';
+import service from './axiosConfig';
+import { getAuthorizationHeader } from './header';
+
+const PAY_PATH = `${BASE_URL}/linepay`;
+
+export const createOrder = async () => {
+  const headers = getAuthorizationHeader();
+  return service.post<{}, Order.OrderAPIResponse>(
+    `${PAY_PATH}`,
+    {},
+    { headers }
+  );
+};
