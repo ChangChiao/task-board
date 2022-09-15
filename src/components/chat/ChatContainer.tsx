b77dd1e448f31434db5b3532f51cd56395337e44
joe <joe.chang1014@gmail.com>
2022-09-15 09:57:37 +0800

    feat chatContainer

---
+++ src/components/chat/ChatContainer.tsx
@@ -0,0 +1,17 @@
+import { useRecoilState } from 'recoil';
+
+import { roomState } from '../../store/room';
+import ChatRoom from './ChatRoom';
+
+const ChatRoomListItem = () => {
+  const [room] = useRecoilState(roomState);
+  return (
+    <div className="fixed bottom-0 hidden w-4/5 pointer-events-none right-10 lg:flex lg:items-end lg:justify-end">
+      {room.map((item: Chat.RoomInfo) => (
+        <ChatRoom key={item.roomId} {...item} />
+      ))}
+    </div>
+  );
+};
+
+export default ChatRoomListItem;
---
+++ src/components/chat/ChatRoom.tsx
@@ -0,0 +1,5 @@
+const ChatRoom = ({ roomId }: Chat.RoomInfo) => {
+  return <div>123 {roomId}</div>;
+};
+
+export default ChatRoom;
