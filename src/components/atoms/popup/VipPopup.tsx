06c881a05398696930f84194d3bc527323f55f28
joe <joe.chang1014@gmail.com>
2022-10-05 15:31:02 +0800

    feat linepay image

--- src/components/atoms/popup/VipPopup.tsx
+++ src/components/atoms/popup/VipPopup.tsx
@@ -1,3 +1,5 @@
+import Image from 'next/image';
+
 import { usePopupContext } from '../../../hooks/usePopupContext';
 import { createOrder } from '../../../utils/http/linePay';
 import PopupTemplate from './PopupTemplate';
@@ -23,6 +25,15 @@ const VipPopup = () => {
           <p className="text-xl font-bold">現在只要299元</p>
           <p>即可成為vip，享有三個月的任務優先曝光!</p>
           <img className="my-6" src="/assets/images/vip.jpg" alt="" />
+          <div className="flex items-center py-2">
+            <span className="pr-1 text-sm">支援付款方式:</span>
+            <Image
+              src="/assets/images/Line_pay_logo.png"
+              width={80}
+              height={30}
+              alt=""
+            />
+          </div>
           <div className="flex justify-between">
             <button className="bg-gray-500 w-36 btn" onClick={closePopup}>
               取消
